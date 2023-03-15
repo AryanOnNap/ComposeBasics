7e599220c0a5dc76ffe06b9ed8aa7f7d7a2fe12a
AryanOnNap <105336048+AryanOnNap@users.noreply.github.com>
2023-03-15 20:02:21 +0530

    todo list app

--- app/src/main/java/com/example/myapplicationcompose/TodoActivity.kt
+++ app/src/main/java/com/example/myapplicationcompose/TodoActivity.kt
@@ -1,16 +1,37 @@
 package com.example.myapplicationcompose
 
+import android.icu.text.CaseMap.Title
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.lifecycleScope
+import com.example.myapplicationcompose.todo.Todo
+import com.example.myapplicationcompose.todoDao.ui.TDViewModel
 import com.example.myapplicationcompose.ui.theme.MyApplicationComposeTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class TodoActivity : ComponentActivity() {
-
+ private val todoViewModel : TDViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -36,13 +57,122 @@ class TodoActivity : ComponentActivity() {
 
             },
             floatingActionButton = {
-                FloatingActionButton(onClick = {}) {
+                val openDialog = remember {
+                    mutableStateOf(false)
+                }
+                FloatingActionButton(onClick = {
+                    openDialog.value = true
+                }) {
+                    CreateAlartDialog(openDialog = openDialog)
                     Icon(Icons.Default.Add, contentDescription = "Add")
                 }
             }
         ) {
+            RecyclerView(todoViewModel = todoViewModel)
         }
 
 
     }
+
+    @Composable
+  fun  CreateAlartDialog(openDialog : MutableState<Boolean>)
+    {
+        val title = remember { mutableStateOf("") }
+        val description = remember { mutableStateOf("") }
+        if(openDialog.value){
+            AlertDialog(onDismissRequest = {},
+                title = {
+                    Text(text = "ToDo")
+                },
+                text = {
+                    Column (
+                        modifier = Modifier
+                            .padding(10.dp)
+                            .fillMaxWidth()
+                    ){
+                        OutlinedTextField(value =title.value , onValueChange = {
+                            title.value = it
+                        },
+                            placeholder = {
+                                Text(text = "Enter Title")
+                            },
+                            label ={
+                                Text(text ="Enter Description")
+                            },
+                            modifier = Modifier.padding(10.dp)
+                        )
+OutlinedTextField(value = description.value,
+    onValueChange = {
+        description.value = it
+    },
+    placeholder = {
+        Text(text = "Enter Description")
+    },
+    label = {
+        Text(text = "Enter description")
+    }, modifier = Modifier.padding(10.dp)
+)
+                    }
+
+
+
+                },
+                confirmButton ={
+                    OutlinedButton(onClick = {
+                        insert(title,description)
+                        openDialog.value = false
+                    }) {
+                        Text(text = "Save")
+
+                    }
+                }
+            )
+        }
+    }
+
+
+    private fun insert(title: MutableState<String>,description : MutableState<String>){
+lifecycleScope.launchWhenCreated {
+    if(!TextUtils.isEmpty(title.value) && TextUtils.isEmpty(description.value)){
+        todoViewModel.insert(
+            Todo(
+                title.value,
+                description.value
+            )
+
+        )
+
+        Toast.makeText(this@TodoActivity,"Inserted Successfully",Toast.LENGTH_SHORT).show()
+
+    }else{
+        Toast.makeText(this@TodoActivity,"please fill all fields",Toast.LENGTH_SHORT).show()
+    }
+}
+    }
+    @Composable
+    fun eachRow(todo: Todo){
+        Card(
+            modifier = Modifier
+                .padding(horizontal = 10.dp, vertical = 10.dp)
+                .fillMaxWidth()
+                , elevation = 4.dp, shape = RoundedCornerShape(4.dp), backgroundColor = Color.Yellow
+
+        ){
+            Column(modifier = Modifier.padding(5.dp)) {
+                Text(text = todo.title, fontWeight = FontWeight.ExtraBold)
+                Text(text = todo.description, fontStyle = FontStyle.Italic)
+            }
+        }
+    }
+@Composable
+fun RecyclerView(todoViewModel: TDViewModel){
+    LazyColumn{
+        items(todoViewModel.responce.value){todo ->
+            eachRow(todo = todo)
+
+        }
+    }
+
+}
+
 }
--- app/src/main/java/com/example/myapplicationcompose/todoDao/ui/TDViewModel.kt
+++ app/src/main/java/com/example/myapplicationcompose/todoDao/ui/TDViewModel.kt
@@ -7,16 +7,20 @@ import androidx.lifecycle.viewModelScope
 import com.example.myapplicationcompose.todo.Todo
 import com.example.myapplicationcompose.todoDao.TodoRepository
 import com.example.myapplicationcompose.util.ErrorUtils
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
+@HiltViewModel
 class TDViewModel
+@Inject
     constructor(private val todoRepository: TodoRepository) : ViewModel(){
     private val TAG = javaClass.simpleName
     val responce :MutableState<List<Todo>> = mutableStateOf(listOf())
         fun insert (todo: Todo) = viewModelScope.launch {
-
+            todoRepository.insert(todo)
         }
     init{
         getAllTodos()
